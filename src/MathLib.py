dd20accf3284e12ae123375740b14412848a14ac
Kilian Testard <pc47xiy@eduvaud.ch>
2024-12-06 12:12:57 +0100

    wip: classe MathLib en cours de construction

--- src/MathLib.py
+++ src/MathLib.py
@@ -1,32 +1,13 @@
-class MockMathRequest:
-
-    def __init__ (self, ope1, ope, ope2):
-        self.ope1 = ope1
-        self.ope = ope
-        self.ope2 = ope2
-        self.result = None
-
-    def get_ope1(self):
-        return self.ope1
-
-    def get_ope(self):
-        return self.ope
-
-    def get_ope2(self):
-        return self.ope2
-
-    def get_res(self):
-        return self.result
-
-    def set_res(self, value):
-        self.result =  value
-
-    def to_string(self):
-        raise NotImplementedError
+from src.MathRequest import MathRequest
 
 class MathLib:
-    @staticmethod
-    def calculate(ope1, ope, ope2):
+    def __init__(self):
+        pass
+
+    def calculate(self, math_request):
+        ope1 = math_request.get_ope1()
+        ope2 = math_request.get_ope2()
+        ope = math_request.get_ope()
         match ope:
             case '+':
                 return ope1 + ope2
@@ -45,15 +26,6 @@ class MathLib:
                 print("Invalid operator.")
                 return None
 
-    @staticmethod
-    def process_request(request):
-        ope1 = request.get_ope1()
-        ope = request.get_ope()
-        ope2 = request.get_ope2()
-
-        result = MathLib.calculate(ope1, ope, ope2)
-
-        request.set_res(result)
-
-        return request
+        math_request.set_res(result)
+        return math_request
 
